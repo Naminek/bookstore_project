73eca70e3d0b7acaf20c431b3bf0c080011137a8
Kambe Namine <nami@koube-no-MacBook-Air.local>
2018-10-30 10:37:47 +0100

    made style simple a bit

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function makeBooksList(data) {
 	for (var i = 0; i < data.length; i++) {
 		var createDiv = document.getElementById("book_list");
 		var oneBook = document.createElement("div");
-		oneBook.innerHTML = '<img src="' + data[i].cover + '"><div class="caption"><p>' + data[i].title + '</p><p>' + data[i].description + '<p><button type="button" onclick="">More Info</button></div>';
+		oneBook.innerHTML = '<img src="' + data[i].cover + '"><div class="caption"><p class="book_title">' + data[i].title + '</p><p class="book_detail text-justify">' + data[i].description + '<p><button type="button" onclick="">More Info</button></div>';
 
 		createDiv.appendChild(oneBook);
 	}
@@ -42,12 +42,12 @@ function searchBooks(data) {
 	for (var i = 0; i < data.length; i++) {
 		if (filterSearch == "") {
 			var oneBook = document.createElement("div");
-			oneBook.innerHTML = '<img src="' + data[i].cover + '"><div class="caption"><p class="book_title">' + data[i].title + '</p><p class="bool_detail">' + data[i].description + '<p><button type="button" onclick="">More Info</button></div>';
+			oneBook.innerHTML = '<img src="' + data[i].cover + '"><div class="caption"><p class="book_title">' + data[i].title + '</p><p class="book_detail text-justify">' + data[i].description + '<p><button type="button" onclick="">More Info</button></div>';
 
 			createDiv.appendChild(oneBook);
 		} else if ((data[i].title.toUpperCase()).indexOf(filterSearch) > -1) {
 			var oneBook = document.createElement("div");
-			oneBook.innerHTML = '<img src="' + data[i].cover + '"><p>' + data[i].title + '</p><p>' + data[i].description + '<p><button type="button" onclick="">More Info</button>';
+			oneBook.innerHTML = '<img src="' + data[i].cover + '"><div class="caption"><p class="book_title">' + data[i].title + '</p><p class="book_detail text-justify">' + data[i].description + '<p><button type="button" onclick="">More Info</button></div>';
 
 			createDiv.appendChild(oneBook);
 		} else if ((data[i].title.toUpperCase()).indexOf(filterSearch) == -1) {
